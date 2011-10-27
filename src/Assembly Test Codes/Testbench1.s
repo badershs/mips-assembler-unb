5d65265318b0d8a14e443fbb8994f72257ce1ec8
geraldes.a.andre@gmail.com <geraldes.a.andre@gmail.com@678479de-c6fe-87d2-aeb7-8d053f141d3a>
2011-10-27 17:50:43 +0000

    Improved the Lexical Analyzer (not ready). Need to treat immediates in any format (binary, octal, decimal, hexadecimal)

--- src/Assembly Test Codes/Testbench1.s
+++ src/Assembly Test Codes/Testbench1.s
@@ -1,11 +1,10 @@
 		li	$t0,0x12345678
 		li	$t1,0x9ABCDEF0
-		add $t2,$t0,$t1	
-		beq $t0,$zero,Error
-		sub $t3,$t2,$t1
-		sll $t4,$t3,2
+		add 	$t2,$t0,$t1	
+		beq 	$t0,$zero,Error
+		sub 	$t3,$t2,$t1
+		sll 	$t4,$t3,2
 		
-		lw	$t5, $
 		
 add:		j	add		
 
--- src/MIPS Assembler/mips_assembler.h
+++ src/MIPS Assembler/mips_assembler.h
@@ -12,23 +12,31 @@
 
 #include <stdint.h>
 
-#define TK_LABEL	(0x00)	
-#define TK_SYMBOL	(0x01)
-#define TK_INST		(0x02)
-#define TK_REG		(0x03)
-#define TK_REG_BRACKET	(0x04)
-#define TK_IMM		(0x05)
+/* ------------------------------ Token types  ----------------------------- */
+#define TK_LABEL	(0x01)	
+#define TK_SYMBOL	(0x02)
+#define TK_INST		(0x03)
+#define TK_REG		(0x04)
+#define TK_REG_ENC	(0x05)
+#define TK_IMM		(0x06)
 
-typedef struct token_s {
-    char* value;
+/* ------------------------------ Error codes  ----------------------------- */
+#define ERR_TK_INV		(0x01)
+#define ERR_TK_REG_INV		(0x02)
+#define ERR_MISA_BRACKET	(0x10)		/* Missaligned Brackets	     */
+#define ERR_MISU_BRACKET	(0x11)		/* Misused Brackets	     */
+
+typedef struct token_st {
+    char* value_s;
+    int16_t value;
     uint8_t type;
-    struct token_s * next;
+    struct token_st * next;
 } token;
 
-typedef struct token_list_s {
+typedef struct token_list_st {
     uint16_t index;
     token* first_token;
-    struct token_list_s * next;
+    struct token_list_st * next;
 } token_list;
 
 #endif

31abcfa9a7fed89bc0c08515523ecad603883d8e
caio.angelo <caio.angelo@678479de-c6fe-87d2-aeb7-8d053f141d3a>
2011-10-29 03:00:47 +0000

    Attention to: Modifications at the defines of sub-types (comment and number) new structs in mips_assembler.h and instructions_table.h Beginning of the creation of the grammar (in instructions_table.c)

--- src/MIPS Assembler/Instructions Table/instructions_table.h
+++ src/MIPS Assembler/Instructions Table/instructions_table.h
@@ -9,25 +9,42 @@
 #define TYPE_J		(0x03)
 
 /* INSTRUCTION SUBTYPES */
+/* Sugiro colocar na ordem crescente a partir do 0, para fazer sentido com a tabela de gramatica criada para no arq .c, se cada um desses subtipos for associado a uma sintaxe especifica  */
+#define NB_STYPE	(0x08)
 #define STYPE_R1	(0x01)
+#define STYPE_R2	(0x02)
+#define STYPE_R3	(0x03)
 #define STYPE_I1	(0x11)
 #define STYPE_I2	(0x12)
 #define STYPE_I3	(0x13)
 #define STYPE_J1	(0x21)
+#define STYPE_J2	(0x22)
 
 /* OPCODES */
 #define OP_ADD		(0x00)
+#define OP_SUB		(0x00)
+#define OP_AND		(0x00)
+#define OP_OR		(0x00)
 #define OP_ADDI		(0x08)
-#define OP_BEQ		(0x04)
 #define OP_LW		(0x23)
+#define OP_SW		(0x1)
+#define OP_BEQ		(0x04)
 #define OP_J		(0x02)
+#define OP_JR		(0x00)
+#define OP_JAL		(0x7)
 
 /* FUNCTS */
 #define FN_ADD		(0x20)
+#define FN_SUB		(0x2)
+#define FN_AND		(0x3)
+#define FN_OR		(0x4)
 #define FN_ADDI		(0x00)
-#define FN_BEQ		(0x00)
 #define FN_LW		(0x00)
+#define FN_SW		(0x00)
+#define FN_BEQ		(0x00)
 #define FN_J		(0x00)
+#define FN_JR		(0x5)
+#define FN_JAL		(0x00)
 
 typedef struct {
     char* name;
@@ -36,5 +53,10 @@ typedef struct {
     uint8_t opcode;
     uint8_t funct;
 } instruction;
+
+typedef struct inst_order_st {
+    uint8_t n;
+    uint8_t *list;
+} inst_order;
 
 #endif

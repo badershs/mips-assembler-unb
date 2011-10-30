984b8a0089c33da12819335d99f21561b151ba03
caio.angelo <caio.angelo@678479de-c6fe-87d2-aeb7-8d053f141d3a>
2011-10-30 15:37:24 +0000

    Some modifications. I'm using now another code to test the parsing code. The grammar was constructed in instructions_table.c. New defines of types of tokens were created to identify different types of registers and immediates at the parsing code

--- trunk/src/MIPS Assembler/instructions_table.c
+++ trunk/src/MIPS Assembler/instructions_table.c
@@ -1,5 +1,6 @@
 #include "instructions_table.h"
 #include "mips_assembler.h"
+#include <stdint.h>
 
 
 const instruction inst_table[] = { 
@@ -18,15 +19,37 @@ const instruction inst_table[] = {
 };
 
 /* This part concerns to create a table of all the rules (grammar) of each one of the NB_STYPE subtypes of instructions */
-int i;
-inst_order inst_order_table[NB_STYPE];
+const uint8_t sintaxe[NB_STYPE][3]={
+	{TK_REG_D,TK_REG_S,TK_REG_T},
+	{TK_REG_S,TK_REG_T,TK_NONE},
+	{TK_REG_S,TK_NONE,TK_NONE},
+	{TK_REG_D,TK_NONE,TK_NONE},
+	{TK_REG_D,TK_REG_T,TK_IMM_5},
+	{TK_REG_D,TK_REG_T,TK_REG_S},
+	{TK_NONE,TK_NONE,TK_NONE},
+	{TK_REG_T,TK_REG_S,TK_IMM_16},
+	{TK_REG_S,TK_REG_T,TK_SYMBOL},
+	{TK_REG_S,TK_IMM_16,TK_REG_ENC},
+	{TK_REG_S,TK_SYMBOL,TK_NONE},
+	{TK_REG_T,TK_IMM,TK_NONE},
+	{TK_SYMBOL,TK_NONE,TK_NONE},
+	{TK_IMM_26,TK_NONE,TK_NONE}
+};
 
     /* STYPE_R1 */
+    /*inst_order[STYPE_R1]={TK_REG,TK_REG,TK_REG};*/
+    /*inst_order[STYPE_R1][0]=TK_REG;*/
+
     /*inst_order_table[STYPE_R1].n=3;
     inst_order_table[STYPE_R1].list=(uint8_t*)malloc(inst_order_table[STYPE_R1].n*sizeof(uint8_t));
     inst_order_table[STYPE_R1].list[0]=TK_REG;
     inst_order_table[STYPE_R1].list[1]=TK_REG;
     inst_order_table[STYPE_R1].list[2]=TK_REG;*/
     /* STYPE_R3 */
+   /* inst_order[STYPE_R3]={TK_REG,NULL,NULL};*/
     /*inst_order_table[STYPE_R2].n=1;
     inst_order_table[STYPE_R2]={TK_REG};*/
+    /*inst_order[STYPE_I1]={TK_REG,TK_REG,TK_IMM};
+    inst_order[STYPE_I2]={TK_REG,TK_REG,TK_SYMBOL};
+    inst_order[STYPE_I3]={TK_REG,TK_IMM;TK_REG};
+    inst_order{STYPE_J1]={TK_SYMBOL,NULL,NULL};*/
